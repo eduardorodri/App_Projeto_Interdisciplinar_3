9a352c67e5f21d54a600dd413dab9a50d7acbedd
Eduardo Rodrigues <101702084+eduardorodri@users.noreply.github.com>
2023-12-02 00:00:27 -0400

    Add files via upload

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,68 +1,91 @@
-import { useState } from 'react'
-import { 
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  FlatList
-} from 'react-native'
-import { useRouter } from 'expo-router'
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList, Platform, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
-const jobsTypes = ["Full-time", "Part-time", "Contractor"]
+
+const jobsTypes = ['Full-time', 'Part-time', 'Contractor'];
+
+const translationMap = {
+  Fulltime: 'Integral',
+  Parttime: 'Meio Período',
+  Contractor: 'Contrato',
+};
 
 const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
-  const [activeJobType, setActiveJobType] = useState('Full-time')
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android') {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permissão para acessar a localização foi negada');
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+      }
+    })();
+  }, []);
+
+  let locationText = 'Aguardando...';
+  if (errorMsg) {
+    locationText = errorMsg;
+  } else if (location) {
+    locationText = JSON.stringify(location);
+  }
 
   return (
-    <View>
-      <View style={styles.container}>
-        <Text style={styles.userName}>Olá DEV</Text>
-        <Text style={styles.welcomeMessage}>Procure vagas de seu interesse...</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.userName}>Olá DEV</Text>
+      <Text style={styles.welcomeMessage}>Procure vagas de seu interesse...</Text>
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput 
+          <TextInput
             style={styles.searchInput}
             value={searchTerm}
             onChangeText={(text) => setSearchTerm(text)}
             placeholder="O que procura?"
           />
         </View>
-        
+
         <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
-          <Image 
-          source={icons.search}
-          resizeMode="contain"
-          style={styles.searchBtnImage}
-          />
+          <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
         </TouchableOpacity>
       </View>
+
       <View style={styles.tabsContainer}>
-        <FlatList 
+        <FlatList
           data={jobsTypes}
           renderItem={({ item }) => (
             <TouchableOpacity
-            style={styles.tab(activeJobType, item)}
-            onPress={() => {
-              setActiveJobType(item);
-              router.push(`/search/${item}`)
-            }}
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
             >
-              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+              <Text style={styles.tabText(activeJobType, item)}>{translationMap[item] || item}</Text>
             </TouchableOpacity>
           )}
-          keyExtractor={item => item}
-          contentContainerStyle={{ columnGap: SIZES.small}}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
         />
       </View>
-    </View>
-  )
-}
 
-export default Welcome
+      {/* Exibir localização */}
+      <Text style={styles.paragraph}>{locationText}</Text>
+    </View>
+  );
+};
+
+export default Welcome;
